c131a64118d7fce9789090b90ccdef5129f5aef5
wkgp <102849982+wklover14@users.noreply.github.com>
2022-05-25 13:05:29 +0200

    Ajout de script d'excécution pour Windows

---
+++ monScript.ps1
@@ -0,0 +1,23 @@
+Write-Output "Hello World, Nous allons installer PAC-MAN sur votre machine"
+gcc --version
+if ( $LASTEXITCODE  -eq 0   ) {
+     <# On lance l'application  #>
+    Get-ChildItem 
+    gcc ./Main.c -o Pac-Man
+    if ( $LASTEXITCODE  -eq 0  ) {
+        <# Action to perform if the condition is true #>
+        Write-Output "Compilation ok.."
+        Start-Sleep (3)
+        Start-Process Pac-Man   
+    } else {
+        Write-Output "Problemes de compilation !! ci dessous un tuto pour l'installer"
+        Write-Output "https://frameboxxindore.com/fr/android/quick-answer-how-do-you-install-gcc-in-windows-10-using-cmd.html"
+    }
+} else {
+    <# On installe gcc  #>
+    Write-Output "Veuillez installez GCC sur la machine"
+} 
+Start-Sleep (5)
+
+
+
